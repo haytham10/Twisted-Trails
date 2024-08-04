be0cd129a1a08d36bf5d0a2618c100a14e538165
haytham10 <cnarabi@gmail.com>
2024-08-05 00:26:11 +0100

    feat: player movement in progress, added trees

--- scripts/ball.gd
+++ scripts/ball.gd
@@ -1,30 +1,39 @@
 extends CharacterBody2D
 
-const SPEED = 180.0
+const MAX_SPEED = 180.0
+const ACCELERATION = 1  # Controls how quickly the ball accelerates to max speed
+const TURN_ANGLE = 0.1  # Adjust for tighter or wider arcs
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
-var gravity = ProjectSettings.get_setting("physics/2d/default_gravity") * 0.6
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity") * 0.1
 
-# Track the current horizontal direction
-var moving_left = true
+# Track the current velocity
+var current_velocity_x = 0.0
 
 func _process(delta):
 	# Check for input on the left or right side of the screen
 	if Input.is_action_just_pressed("move_left"):
-		moving_left = true
+		current_velocity_x = -MAX_SPEED
 	elif Input.is_action_just_pressed("move_right"):
-		moving_left = false
+		current_velocity_x = MAX_SPEED
+
+	# Gradually increase or decrease the speed to the target velocity
+	if current_velocity_x < 0:
+		current_velocity_x = max(-MAX_SPEED, current_velocity_x - ACCELERATION * delta)
+	elif current_velocity_x > 0:
+		current_velocity_x = min(MAX_SPEED, current_velocity_x + ACCELERATION * delta)
 
 func _physics_process(delta):
 	# Add gravity to the vertical velocity
 	if !is_on_floor():
 		velocity.y += gravity * delta
 
-	# Set horizontal velocity based on the current direction
-	if moving_left:
-		velocity.x = -SPEED
-	else:
-		velocity.x = SPEED
+	# Calculate the angle of movement for the arc effect
+	var angle = current_velocity_x * TURN_ANGLE * delta
+
+	# Update the horizontal velocity to create a smooth arc
+	velocity.x = current_velocity_x * cos(angle)
+	velocity.y += current_velocity_x * sin(angle)
 
 	# Move the character
 	move_and_slide()
